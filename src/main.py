530fe63abbf03f8841625f9140b77e7b8aabb355
0xMere <merenda.s.mattia@gmail.com>
2024-11-24 11:32:20 +0100

    Prima bozza per la scrittura di atomi su file (#4)
    - Aggiunti docstring dettagliati per tutti i metodi della classe , spiegando i parametri, i valori restituiti e le eccezioni sollevate.
    - La funzione  ora accetta un dataset opzionale come parametro e filtra i dati basandosi su colonne specifiche.
    - Aggiunta la gestione dell'errore  nella funzione  per formati di file non validi.
    - Migliorata la gestione degli errori nelle funzioni di lettura e salvataggio dei dati.
    - Eliminata la vecchia classe  poiché è stata sostituita dalla classe  con nuove funzionalità.
    - Aggiornato il codice di  per utilizzare  invece di , con nuovi metodi di test per il caricamento, filtraggio e scrittura dei dati.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-from modules.loader import Loader
+from modules.dataset_manager import DatasetManager
 from modules.department_parser import DepartmentParser
 from modules.dataset_loader import DatasetLoader
 
-def main():
-    # Inizializza il Loader
-    loader = Loader()
+def test_parser():
+    # Inizializza il Dataset Manager
+    dataset_manager = DatasetManager()
 
     # Ottiene la lista dei dipartimenti
-    departments = loader.get_departments()
+    departments = dataset_manager.get_departments()
     if not departments:
         print("Errore: nessun file trovato nella cartella dataset.")
         return
@@ -21,18 +21,18 @@ def main():
     if args.all:
         print("Caricamento dati per tutti i dipartimenti...")
         for dep in departments:
-            loader.load_data(dep)
+            dataset_manager.load_data(dep)
     else:
         for dep in departments:
             if getattr(args, dep, False):
-                loader.load_data(dep)
+                dataset_manager.load_data(dep)
 
     # Se nessun argomento è specificato
     if not any(vars(args).values()):
         print("Errore: nessun dipartimento selezionato.")
         parser.parser.print_help()
 
-def test_dataset_loader():
+def test_filtra_per_valori():
     dataset_loader = DatasetLoader('dataset/coperture.xlsx')
 
     filters = {
@@ -43,6 +43,40 @@ def test_dataset_loader():
 
     dataset_loader.save_to_file(df=filtered_data, output_file_path='dataset/tmp_info.csv')
 
+def test_filtra_per_colonne():
+    dataset_loader = DatasetLoader('dataset/coperture.xlsx')
+
+    filters = {
+        'Cod. Tipo Corso': ['LM'],
+        'SSD': ['INF/01']
+    }
+    data = dataset_loader.filter_by_values(filters=filters)
+
+    columns = [
+        'Cognome',
+        'Matricola',
+        'Cod. Att. Form.', # ID insegnamento
+        'Des. Insegnamento',
+        'CFU'
+    ]
+    data = dataset_loader.get_values(dataset=data, columns=columns)
+    print(data)
+
+def test_scrittura_atomi():
+    dataset_loader = DatasetLoader('dataset/coperture.xlsx')
+
+    filters = {
+        # 'Cod. Tipo Corso': ['LM'],
+        'SSD': ['INF/01']
+    }
+    data = dataset_loader.filter_by_values(filters=filters)
+
+    dataset_manager = DatasetManager()
+    dataset_manager.write_atoms(data, 'informatica')
+
 if __name__ == "__main__":
-    # main()
-    test_dataset_loader()
+    # test_parser()
+    # test_filtra_per_valori()
+    # test_filtra_per_colonne()
+    test_scrittura_atomi()
+    
--- src/modules/dataset_loader.py
+++ src/modules/dataset_loader.py
@@ -2,28 +2,39 @@ import openpyxl
 import pandas as pd
 
 class DatasetLoader:
+    """
+    Classe per la gestione del caricamento, filtraggio e salvataggio dei dati da file Excel o CSV.
+    """
+
     def __init__(self, input_file_path="dataset/coperture.xlsx"):
         """
-        Inizializza la classe DatasetLoader con il percorso al datset.
-        :param input_file_path: Path relativo al file da leggere.
+        Inizializza la classe DatasetLoader con il percorso al dataset.
+        :param input_file_path: Path relativo al file Excel da leggere (default: "dataset/coperture.xlsx").
         """
         self.input_file_path = input_file_path
     
-    def get_from_database(self, columns=None):
+    def get_values(self, dataset=None, columns=None):
         """
-        Carica un file Excel e filtra per colonne specifiche se fornito.
-        :param columns: Lista di colonne da mantenere (es. ["Matricola", "SSD 2015"]).
-        :return: DataFrame filtrato.
+        Carica un file Excel o utilizza un dataset esistente, filtrando opzionalmente per colonne specifiche.
+        :param dataset: DataFrame esistente da utilizzare (default: None). Se None, viene caricato il file Excel.
+        :param columns: Lista di colonne da mantenere (esempio: ['Matricola', 'Cognome']).
+        :return: DataFrame contenente tutte le righe, filtrato per le colonne specificate (se fornite).
+        :raises ImportError: Se il pacchetto openpyxl non è installato.
+        :raises KeyError: Se una o più colonne specificate non esistono nel dataset.
         """
         try:
-            # Legge l'intero file
-            df = pd.read_excel(self.input_file_path, engine="openpyxl")
-            df = pd.read_excel(self.input_file_path, sheet_name=0, header=0, usecols=None, nrows=None)
+
+            if dataset is None:
+                # Legge l'intero file
+                df = pd.read_excel(self.input_file_path, engine="openpyxl")
+                df = pd.read_excel(self.input_file_path, sheet_name=0, header=0, usecols=None, nrows=None)
+            else:
+                df = dataset
             
             # Filtraggio sulle colenne desiderate
             if columns:
                 df = df[columns]
-                print(df)
+                # print(df)
             return df
         except ImportError as e:
             print("Errore: il pacchetto 'openpyxl' non è installato.")
@@ -32,16 +43,23 @@ class DatasetLoader:
             print(f"Errore: una o più colonne specificate non esistono nel file. {e}")
             raise e
     
-    def filter_by_values(self, filters):
+    def filter_by_values(self, filters, dataset=None):
         """
         Filtra i dati in base a più valori specifici in diverse colonne, convertendo tutti i valori in stringhe.
         :param filters: Dizionario in cui le chiavi sono i nomi delle colonne e i valori sono liste di valori da filtrare.
-                        Esempio: {'Cod. Tipo Corso': ['LM'], 'SSD': ['INF/01', 'MAT/05']}
-        :return: DataFrame filtrato.
+                        Esempio: {'Cod. Tipo Corso': ['LM'], 'SSD': ['INF/01', 'MAT/05']}.
+        :param dataset: DataFrame esistente da utilizzare (default: None). Se None, viene caricato il file Excel.
+        :return: DataFrame contenente solo le righe che soddisfano i criteri di filtraggio.
+        :raises ImportError: Se il pacchetto openpyxl non è installato.
+        :raises KeyError: Se una delle colonne specificate nei filtri non esiste nel dataset.
+        :raises Exception: Per altri errori durante il filtraggio.
         """
         try:
-            # Carica l'intero dataset
-            df = pd.read_excel(self.input_file_path, engine="openpyxl")
+            if dataset is None:
+                # Carica l'intero dataset
+                df = pd.read_excel(self.input_file_path, engine="openpyxl")
+            else:
+                df = dataset
             
             # Converte tutti i valori del DataFrame in stringhe
             df = df.astype(str)
@@ -75,6 +93,8 @@ class DatasetLoader:
         :param df: Il DataFrame da salvare.
         :param output_file_path: Percorso del file di output.
         :param file_format: Formato del file, può essere 'csv' o 'excel'.
+        :raises ValueError: Se il formato specificato non è 'csv' o 'excel'.
+        :raises Exception: Per altri errori durante il salvataggio del file.
         """
         try:
             if file_format == "csv":
